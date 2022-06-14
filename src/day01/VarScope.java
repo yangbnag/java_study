65603b0b63b79a35ac24dc1ce3a3ee7e81036ff4
yanbnag <yanbam@naver.com>
2022-06-15 00:18:19 +0900

    22.06.15_home

--- src/day01/VarScope.java
+++ src/day01/VarScope.java
@@ -6,18 +6,18 @@ public class VarScope {
         // 특정 블록에서 선언된 변수는 해당 블록을 벗어나면
         // 메모리에서 자동 삭제됨
 
-        if (true) {
         int num3; // 문제 발생시 alt + Enter를 누르면 문제 제시 인텔리제이 기능.
+        if (true) {
             num1 = 300; // 상위 블록에 존재하는 동일한 변수명 선언 불가.
             num3 = num1 + num2;
             System.out.println("num3 = " + num3);
         } // end if
 
-//        System.out.println("num3 = " + num3);
+        System.out.println("num3 = " + num3);
         System.out.println("num1 = " + num1);
 
         int i = 5;
-        int num3 = 500; // if문을 나오면서 num3는 사라지기 때문에 이 영역에서 num3 변수 선언 가능
+//        int num3 = 500; // if문을 나오면서 num3는 사라지기 때문에 이 영역에서 num3 변수 선언 가능
         System.out.println(num3);
     } // end main
 
