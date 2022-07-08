934cc9ca924ebd0c8994a16092c2947acfda5a50
yangbnag <yanbam@naver.com>
2022-07-08 18:18:17 +0900

    22.07.08

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Child c = new Child();
+        int id = 1;
+        int pw = 2;
+
+        int testId = 1;
+        int testPw = 2;
+
+        if (id != testId){
+            System.out.println("아이디가 틀립니다.");
+        } else if (pw != testPw) {
+            System.out.println("비밀번호가 틀립니다.");
+        } else System.out.println("로그인 성공");
+
 
 
     }
